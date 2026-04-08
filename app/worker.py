dcf8fa4a2fbf3a712226763b61b94181d7ed8a50
Eugene Gulyaev <gulyaeve@vk.com>
2026-04-08 16:21:46 +0300

    test without module docker

--- app/worker.py
+++ app/worker.py
@@ -1,35 +1,37 @@
 import logging
 
-import docker
+# import docker
+import subprocess
 import httpx
 
 from app.settings import settings
 from app.schemas import ScheduleScheme
 
 
-docker_client = docker.from_env()
-# Define the GPU request
-gpu_request = docker.types.DeviceRequest(
-    count=-1,           # -1 is equivalent to 'all'
-    capabilities=[['gpu']] # Requests the 'gpu' capability
-)
+# docker_client = docker.from_env()
+# # Define the GPU request
+# gpu_request = docker.types.DeviceRequest(
+#     count=-1,           # -1 is equivalent to 'all'
+#     capabilities=[['gpu']] # Requests the 'gpu' capability
+# )
 
 
 def run_task(schedule: ScheduleScheme) -> str:
-    container = docker_client.containers.run(
-        image="video-ai",
-        detach=False,
-        volumes={
-            f'/mnt/records/{schedule.date}/{schedule.id}_{schedule.camera_id}.mkv': {'bind': f'/app/videos/{schedule.id}_{schedule.camera_id}.mkv', 'mode': 'rw'},
-            '/home/admin/video_ai/output': {'bind': '/app/output', 'mode': 'rw'},
-            '/home/admin/video_ai/models': {'bind': '/app/models', 'mode': 'ro'},
-        },
-        remove=True,
-        device_requests=[gpu_request],
-        stdout=True,
-        stderr=True,
-    )
-    print(str(container))
+    # container = docker_client.containers.run(
+    #     image="video-ai",
+    #     detach=False,
+    #     volumes={
+    #         f'/mnt/records/{schedule.date}/{schedule.id}_{schedule.camera_id}.mkv': {'bind': f'/app/videos/{schedule.id}_{schedule.camera_id}.mkv', 'mode': 'rw'},
+    #         '/home/admin/video_ai/output': {'bind': '/app/output', 'mode': 'rw'},
+    #         '/home/admin/video_ai/models': {'bind': '/app/models', 'mode': 'ro'},
+    #     },
+    #     remove=True,
+    #     device_requests=[gpu_request],
+    #     stdout=True,
+    #     stderr=True,
+    # )
+    # print(str(container))
+    subprocess.run(["start_ai.py", "--date", f"{schedule.date}", "--id", f"{schedule.id}_{schedule.camera_id}"])
     with open(f"/home/admin/video_ai/output/{schedule.id}_{schedule.camera_id}/summary.txt") as file:
         summary = file.read()
     data = {
